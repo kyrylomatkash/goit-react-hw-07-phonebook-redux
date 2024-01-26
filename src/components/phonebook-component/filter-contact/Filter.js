ac9984b53f7858841657911704e6715f83004594
Kyrylo <kyrylo.mm@gmail.com>
2024-01-26 05:02:03 +0100

    Update Filter.js

--- src/components/phonebook-component/filter-contact/Filter.js
+++ src/components/phonebook-component/filter-contact/Filter.js
@@ -1,12 +1,14 @@
-// Імпорт бібліотек,стилів і логіки Redux
+// Імпорт компонентів,бібліотек,Redux логіки і селекторів
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { filterAction } from '../../../redux/filterSlice';
+import { selectFilter } from '../../../redux/selectors';
+
 import { AppContainer, SearchInput, Heading } from './filterstyles';
 // Основна функція компоненту
 const Filter = () => {
   const dispatch = useDispatch();
-  const filter = useSelector(state => state.filter.filter);
+  const filter = useSelector(selectFilter);
   // Зміна значення фільтра
   const handleChange = e => {
     dispatch(filterAction(e.target.value));
