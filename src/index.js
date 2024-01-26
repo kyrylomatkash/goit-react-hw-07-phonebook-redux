e8a4de82c2316a4780bbf1f31354060442585f38
Kyrylo <kyrylo.mm@gmail.com>
2024-01-26 04:50:17 +0100

    Update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-// Імпорт бібліотек,стилів і логіки Redux
+// Імпорт бібліотек,стилів і Redux логіки
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from 'components/App';
 import { store } from './redux/store';
 
+// Коренева система проєкту
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
